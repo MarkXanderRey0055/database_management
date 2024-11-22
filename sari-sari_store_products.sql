4f2171f9f0f4a4d841b6b8cba6d805f899130b93
MarkXanderRey0055 <23-10462@g.batstate-u.edu.ph>
2024-11-22 13:31:55 +0800

    Add files via upload

---
+++ sari-sari_store_products.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE convenience_store1 (
+    ID INT PRIMARY KEY,
+    Product_Name VARCHAR(255),
+    Price DECIMAL(10, 2),
+    Unit VARCHAR(50),
+    Category VARCHAR(50)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
